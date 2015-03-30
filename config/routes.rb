f105d177cb92495968ed059096fcb6abb0e835d1
ciregbulem <chudi.iregbulem@trincoll.edu>
2015-03-29 22:19:22 -0400

    Reverted Code Back to Working Condition

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'
-      } do
-    get 'logout' => 'users/sessions#destroy'
-  end
+      }
   resources :users
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   get 'home/about'
