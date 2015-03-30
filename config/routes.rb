fa156e0a18bd08bbe2287ac376dbbcf0dbe36e41
ciregbulem <chudi.iregbulem@trincoll.edu>
2015-03-29 22:07:06 -0400

    Fixing Sign Out for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'
-      } do
-        get 'logout' => 'devise/sessions#destroy'
+      }
+  devise_for :users do
+    get 'users/sign_out' => 'devise/sessions#destroy'
   end
   resources :users
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
