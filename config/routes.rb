c3eb73fe61e17183e22309c4a268b490f961de6c
ciregbulem <chudi.iregbulem@trincoll.edu>
2015-03-29 22:00:38 -0400

    Added GET route for deleting user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'
-      }
+      } do
+        get 'logout' => 'devise/sessions#destroy'
+  end
   resources :users
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   get 'home/about'
